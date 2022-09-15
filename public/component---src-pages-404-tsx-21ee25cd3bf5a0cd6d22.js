759315accf013757ff379349d6954857acd39039
WonWonGit <bkn020612@gmail.com>
2022-09-15 18:25:01 +0900

    initail commit

---
+++ public/component---src-pages-404-tsx-21ee25cd3bf5a0cd6d22.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[218],{2513:function(e,t,n){n.r(t);var r=n(7294);t.default=function(){return r.createElement("div",null,"error")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-21ee25cd3bf5a0cd6d22.js.map
